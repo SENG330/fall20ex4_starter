4675d4c3eab4d84262fb85051cdf0034619a600d
Neil Ernst <neil@neilernst.net>
2020-10-09 15:24:23 -0700

    test CI

--- src/main/java/ca/uvic/seng330/ex4/Driver.java
+++ src/main/java/ca/uvic/seng330/ex4/Driver.java
@@ -9,6 +9,8 @@ public class Driver {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Driver().getGreeting());
+
+      System.out.println(new Driver().getGreeting());
+      Singleton s = Singleton.getInstance();
     }
 }
